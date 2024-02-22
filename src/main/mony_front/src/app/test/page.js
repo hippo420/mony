3efa30860eddfaeacc98108844aa9d79148f6b73
gaebabja <hippo420@naver.com>
2024-02-22 12:48:57 +0900

    이벤트 색상

--- src/main/mony_front/src/app/test/page.js
+++ src/main/mony_front/src/app/test/page.js
@@ -1,13 +1,21 @@
+'use client'
+import MyCalendar from '../components/MyCalendar';
+import EventDetail from '../components/EventDetail';
+import { useState } from 'react';
 
 export default function Test() {
-    const port = process.env.NEXT_PUBLIC_PORT;
-    console.log('ENV: '+ port);
-    
-    return (
-    <div>
-        <h2>Test</h2>
+const [isCalendarExpanded, setCalendarExpanded] = useState(true);
+
+  const toggleCalendarSize = () => {
+    alert('달려클릭');
+    setCalendarExpanded(!isCalendarExpanded);
+  };
+
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <MyCalendar/>
+      <EventDetail onClick={toggleCalendarSize} isVisible={!isCalendarExpanded} />
     </div>
-    );
-    
-}
-    
+  );
+                       
+};
